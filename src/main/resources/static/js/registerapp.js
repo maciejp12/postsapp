273957925d8c864227e1b84bb8c0fd2e7d76e652
maciejp12 <maciejpredon@gmail.com>
2020-12-18 16:52:59 +0100

    Added confirm password field to register template

--- src/main/resources/static/js/registerapp.js
+++ src/main/resources/static/js/registerapp.js
@@ -1,4 +1,6 @@
 const url = 'http://127.0.0.1:8080/api/user/process_register';
+const passwdNotMatchMessage = 'password doesn\'t match';
+const passwdMatchMessage = 'matching';
 
 var getRegisterInput = () => {
     //TODO validate data
@@ -6,16 +8,39 @@ var getRegisterInput = () => {
     let email = document.getElementById('email').value;
     let name = document.getElementById('name').value;
     let password = document.getElementById('password').value;
+    let passwordConfirm = document.getElementById('confirm_password').value;
 
     return {'email' : email,
             'name' : name,
-            'password' : password
+            'password' : password,
+            'password_confirm' : passwordConfirm
     }
 }
 
-function submitRegisterForm(){
+var checkPassword = () => {
+    let message = document.getElementById('password_message');
+    let password = document.getElementById('password');
+    let passwordConfirm = document.getElementById('confirm_password');
+
+    if (password.value == passwordConfirm.value) {
+        message.innerHTML = passwdMatchMessage;
+        message.style.color = 'green';
+    } else {
+        message.innerHTML = passwdNotMatchMessage;
+        message.style.color = 'red';
+    }
+}
+
+var submitRegisterForm = () => {
     var xhr = new XMLHttpRequest();
     var json = getRegisterInput();
+
+    if (json.password != json.password_confirm) {
+        let passwordMessage = document.getElementById('password_message');
+        passwordMessage.innerHTML = passwdNotMatchMessage;
+        passwordMessage.style.color = 'red';
+        return;
+    }
 
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
