03676eed5c28e0afbf707e7d628c6b3ff9a6c954
maciejp12 <maciejpredon@gmail.com>
2020-12-19 16:20:09 +0100

    Added css to newpost template and post creation forntend data validation

--- src/main/resources/static/js/newpostapp.js
+++ src/main/resources/static/js/newpostapp.js
@@ -2,7 +2,6 @@ const url = 'http://127.0.0.1:8080/api/posts';
 
 var getInput = () => {
 
-    //TODO validate data
 
     let title = document.getElementById('title').value;
     let content = document.getElementById('content').value;
@@ -11,10 +10,46 @@ var getInput = () => {
             'content' : content};
 }
 
-function submitform(){
+const minTitleLen = 3;
+const maxTitleLen = 64;
+
+const minContentLen = 3;
+const maxContentLen = 4096;
+
+var validateTitle = (title) => {
+    let len = title.length;
+    return len >= minTitleLen && len <= maxTitleLen;
+}
+
+var validateContent = (content) => {
+    let len = content.length;
+    return len >= minContentLen && len <= maxContentLen;
+}
+
+var validateInput = (input) => {
+    let errorMessage = document.getElementById('post_error');
+
+    if (!validateTitle(input.title)) {
+        errorMessage.innerHTML = 'title must be between 3 and 64 characters long';
+        return false;
+    }
+
+    if (!validateContent(input.content)) {
+        errorMessage.innerHTML = 'content must be between 3 and 4096 characters long';
+        return false;
+    }
+
+    return true;
+}
+
+var submitform = () => {
     var xhr = new XMLHttpRequest();
     var json = getInput();
 
+    if (!validateInput(json)) {
+        return;
+    }
+    
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.onreadystatechange = () => {
